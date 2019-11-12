c8ffebe7cbf8edcd558d7ab66380f25fa3dd5470
Thiago Malaquias <thiago.malaca@gmail.com>
2019-11-12 02:33:33 -0300

    add bacen

---
+++ Bacen/Entities/Cotacao.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Bacen.Entities
+{
+    public class Cotacao
+    {
+        public DateTime Data { get; set; }
+
+        public double Valor { get; set; }
+
+        public string Moeda { get; set; }
+    }
+}
---
+++ Bacen/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using Serilog;
+
+namespace Bacen
+{
+    public class Program
+    {
+
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory() + "/config/")
+            .AddJsonFile("appsettings.json", optional : false, reloadOnChange : true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional : false, reloadOnChange : true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static void Main(string[] args)
+        {
+            /* Adicionado a estrutura do try/catch abaixo para captura os erros de inicialização,
+               erros que normalmente geram dor de cabeça para resolver. */
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory() + "/config/");
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
