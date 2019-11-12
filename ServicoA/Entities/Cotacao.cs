744ab510db4e5fddfb4b39adb1b658a01c427cbc
Thiago Malaquias <thiago.malaca@gmail.com>
2019-11-12 02:41:24 -0300

    add servicoA

---
+++ ServicoA/Entities/Cotacao.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ServicoA.Entities
+{
+    public class Cotacao
+    {
+        public DateTime Data { get; set; }
+
+        public double Valor { get; set; }
+
+        public string Moeda { get; set; }
+    }
+}
